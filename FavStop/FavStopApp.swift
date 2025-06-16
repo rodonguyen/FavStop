463e30e27ecfd6a0b903dcee3b7c89da0b120349
Rodo <rodonguyendd@gmail.com>
2025-06-16 10:09:53 +1000

    Initial Commit

---
+++ FavStop/FavStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavStopApp.swift
+//  FavStop
+//
+//  Created by Rodo Nguyen on 16/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct FavStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FavStopTests/FavStopTests.swift
@@ -0,0 +1,17 @@
+//
+//  FavStopTests.swift
+//  FavStopTests
+//
+//  Created by Rodo Nguyen on 16/6/2025.
+//
+
+import Testing
+@testable import FavStop
+
+struct FavStopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
